cc9985a43f63784877b7b23c706a241e7a57a3b0
scarymen1325 <kyslenko3@outlook.com>
2021-08-12 22:17:51 +0300

    miatoll: added Google Calculator
    * Calculator app not included in vanilla build

--- miatoll.mk
+++ miatoll.mk
@@ -62,6 +62,10 @@ PRODUCT_PACKAGES += \
     BromiteWV \
     Bromite
 
+# Calc
+PRODUCT_PACKAGES += \
+    Calc
+
 # Camera
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
